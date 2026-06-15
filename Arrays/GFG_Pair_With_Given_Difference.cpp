9e98bd421f369eac32ad0125947f9049b2a88fc5
Raushik-7 <raushik7366@gmail.com>
2026-06-15 12:22:59 +0530

    Implement findPair to check for pairs with difference

---
+++ Arrays/GFG_Pair_With_Given_Difference.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+  public:
+    bool findPair(vector<int> &arr, int x) {
+        // code here
+        sort(arr.begin(),arr.end());
+        int n= arr.size();
+        int start =0,end=1;
+        if(x<0){
+            x=x*-1;
+        }
+        while(end<n){
+            if(arr[end]-arr[start]==x){
+                return true;
+            }
+            else if(arr[end]-arr[start]<x){
+                end++;
+            }
+            else{
+                start++;
+            }
+            if(start==end){
+                end++;
+            }
+        }
+        return false;
+    }
+};
